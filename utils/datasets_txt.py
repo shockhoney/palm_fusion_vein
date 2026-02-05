dc5014ab34ca3337109a7fd3ddfd2d039653ec89
shockhoney <1710669647@qq.com>
2026-02-05 13:47:58 +0800

    1

--- utils/datasets_txt.py
+++ utils/datasets_txt.py
@@ -176,8 +176,8 @@ class PairTxtDataset(Dataset):
 #     phase2_list(root_dir, train_txt, val_txt, val_ratio=0.2)
 
 if __name__ == '__main__':
-     gen_polyu_list("/data-ext-c/palm_fusion_vein/data/PolyU/Red",
-    out_txt="PolyU_Red_list.txt",
+     gen_polyu_list("/data-ext-c/palm_fusion_vein/data/PolyU/NIR",
+    out_txt="PolyU_NIR_list.txt",
     train_ratio=0.8,
     val_ratio=0.1,
     seed=42)
